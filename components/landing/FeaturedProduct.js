ffc40bafff8ed5e715b078474938bb0c00c516cf
SankhaChak <anonproductionspro@gmail.com>
2022-04-11 05:12:55 +0530

    enhancement: ⛲ Added animation on mount

--- components/landing/FeaturedProduct.js
+++ components/landing/FeaturedProduct.js
@@ -6,7 +6,7 @@ const FeaturedProduct = ({ id, name, imgSrc, similarProducts }) => {
   return (
     <div className="col-span-6 grid grid-cols-1 md:grid-cols-8">
       <div className="md:col-span-2 order-2 md:order-1 relative z-40 flex flex-col justify-center">
-        <h1 className="text-2xl md:text-4xl font-bold text-center md:text-left flex-1 md:flex md:items-center">
+        <h1 className="text-2xl md:text-4xl font-bold text-center md:text-left flex-1 md:flex md:items-center product-name">
           {name}
         </h1>
         <div className="flex justify-center items-center md:justify-start gap-4 py-8">
@@ -37,10 +37,14 @@ const FeaturedProduct = ({ id, name, imgSrc, similarProducts }) => {
         </div>
       </div>
       <div className="md:col-span-4 relative order-1 md:order-2">
-        <img className="md:absolute md:bottom-0 z-10" src={imgSrc} alt={name} />
+        <img
+          className="md:absolute md:bottom-0 z-10 featured-image"
+          src={imgSrc}
+          alt={name}
+        />
         <span className="hidden md:block bg-blue-400 opacity-50 h-[36rem] w-[36rem] absolute top-1/4 left-1/2 transform -translate-x-1/2 rounded-full" />
         <Link href={`/products/${id}`}>
-          <a className="absolute top-1/2 left-1/2 transform -translate-x-1/2 md:left-1/4 md:translate-x-0 bg-slate-300 text-black p-4 rounded-full z-20">
+          <a className="absolute top-1/2 left-1/2 transform -translate-x-1/2 md:left-1/4 md:translate-x-0 bg-slate-300 text-black p-4 rounded-full z-20 product-link origin-center">
             <PlusIcon className="w-6" />
           </a>
         </Link>
--- components/layout/landing/Hero.js
+++ components/layout/landing/Hero.js
@@ -1,3 +1,6 @@
+import { useEffect } from "react";
+import { gsap } from "gsap";
+
 import { ChevronRightIcon } from "../../icons";
 import FeaturedProduct from "../../landing/FeaturedProduct";
 import NextProduct from "../../landing/NextProduct";
@@ -5,6 +8,73 @@ import NextProduct from "../../landing/NextProduct";
 const Hero = ({ featuredProduct, products }) => {
   const { id, name, imgSrc, similarProducts } = featuredProduct;
   const nextProduct = products[0];
+
+  useEffect(() => {
+    animateOnMount();
+  }, []);
+
+  const animateOnMount = () => {
+    gsap.fromTo(
+      ".featured-image",
+      {
+        opacity: 0,
+        y: "8rem",
+        duration: 0.8,
+      },
+      {
+        opacity: 1,
+        y: "0",
+        duration: 0.8,
+      }
+    );
+
+    gsap.fromTo(
+      ".products",
+      {
+        opacity: 0,
+        y: "-8rem",
+        duration: 0.8,
+      },
+      {
+        opacity: 1,
+        y: "0",
+        duration: 0.8,
+      }
+    );
+
+    gsap.fromTo(
+      ".product-name",
+      {
+        opacity: 0,
+        x: "-8rem",
+        duration: 0.8,
+      },
+      {
+        opacity: 1,
+        x: "0",
+        duration: 0.8,
+      }
+    );
+
+    gsap.fromTo(
+      ".product-link",
+      {
+        scale: 0,
+        x: "-8rem",
+        duration: 0.8,
+      },
+      {
+        scale: 1.5,
+        x: "0",
+        duration: 0.8,
+      },
+      {
+        scale: 1,
+        x: "0",
+        duration: 0.8,
+      }
+    );
+  };
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-9 flex-1">
